cc042ada0f7791b67cc25ea3421066aed816edbc
alebo <alebo62@yandex.ru>
2025-01-08 14:37:25 +0300

    Игра готова

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -12,22 +12,41 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        int tenthsOfSecondEllapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
 
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(.1);
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+
             SetUpGame();
             
+        }
+        
+        private void DispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            tenthsOfSecondEllapsed++;
+            timerTextBlock.Text = (tenthsOfSecondEllapsed / 10F).ToString("0.0s");
+            if(matchesFound == 8)
+            {
+                dispatcherTimer.Stop();
+                timerTextBlock.Text = timerTextBlock.Text + " - Play Again?";
+            }
         }
 
         private void SetUpGame()
         {
-            List<string> animalEmoji = new List<string>()
+            matchesFound = 0;
+            List<string> animalEmoji = new()
             {
                 "🙊", "🙊",
                 "🐔", "🐔",
@@ -41,13 +60,54 @@ namespace MatchGame
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                Random random = new Random();
+                textBlock.Visibility = Visibility.Visible;
+                if (textBlock.Text != timerTextBlock.Text)
+                {
+                    Random random = new();
 
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
             }
+
+            dispatcherTimer.Start();
+        }
+
+        TextBlock lastTB;
+        bool firstClick = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock currentTB = sender as TextBlock;
+
+            if (firstClick == false)
+            {
+                lastTB = currentTB;
+                firstClick = true;
+                currentTB.Visibility = Visibility.Hidden;
+            }
+            else if (lastTB.Text == currentTB.Text)
+            {
+                firstClick = false;
+                currentTB.Visibility = Visibility.Hidden;
+                matchesFound++;
+            }
+            else
+            {
+                lastTB.Visibility = Visibility.Visible;
+                firstClick = false;
+            }
+        }
+        
+        private void timerTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+                tenthsOfSecondEllapsed = 0;
+            }
+
         }
     }
 }
--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
+        enum WeekDays { One, Two, Three };
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string[] sDays = { "Понедельник", "Вторник", "Среда" };
+            WeekDays days = WeekDays.Two;
+            days++;
+            Console.WriteLine(days);
+            Console.WriteLine(sDays[(int)days]);
+
             Console.ReadLine();
         }
     }
